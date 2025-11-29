0287b8a6092f5b54433c50c76ba2f7664b7fed13
zduyguortancil <duygu.ortancil@gmail.com>
2025-11-29 13:33:37 +0300

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,22 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL =
+        "jdbc:mysql://localhost:3306/cmpe343_project2?useSSL=false&allowPublicKeyRetrieval=true";
+    private static final String USER = "root";
+    private static final String PASSWORD = "KLJHOUY986104__";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
